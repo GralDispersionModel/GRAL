b46dde434fcbbe07e7320d0450eb2a815e6549dc
Markus <markus.kuntner@tirol.gv.at>
2020-05-22 21:36:52 +0200

    New struct for simple int. Points

--- src/Point.cs
+++ src/Point.cs
@@ -12,36 +12,36 @@
 
 namespace GRAL_2001
 {
-	///<summary>
-	///Point structure for integer values
-	///</summary>
-	public struct IntPoint
-	{
-		public int X;
-		public int Y;
+    ///<summary>
+    ///Point structure for integer values
+    ///</summary>
+    public struct IntPoint
+    {
+        public int X;
+        public int Y;
 
-		public IntPoint(int x, int y) 
-		{
-			X = x;
-			Y = y;
-		}
-		
-		public override bool Equals(object obj) 
-		{
-			return obj is IntPoint && this == (IntPoint)obj;
-		}
-		public override int GetHashCode() 
-		{
-			return X.GetHashCode() ^ Y.GetHashCode();
-		}
-		public static bool operator ==(IntPoint a, IntPoint b) 
-		{
-			return a.X == b.X && a.Y == b.Y;
-		}
-		public static bool operator !=(IntPoint a, IntPoint b) 
-		{
-			return !(a == b);
-		}
-	}
+        public IntPoint(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is IntPoint && this == (IntPoint)obj;
+        }
+        public override int GetHashCode()
+        {
+            return X.GetHashCode() ^ Y.GetHashCode();
+        }
+        public static bool operator ==(IntPoint a, IntPoint b)
+        {
+            return a.X == b.X && a.Y == b.Y;
+        }
+        public static bool operator !=(IntPoint a, IntPoint b)
+        {
+            return !(a == b);
+        }
+    }
 
 }
