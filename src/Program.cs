cd0cc9b93e60c063ee6fc3ae1795c2590bd99fff
Markus <markus.kuntner@tirol.gv.at>
2023-12-12 17:59:32 +0100

    Set version to 23.11

--- src/Program.cs
+++ src/Program.cs
@@ -66,7 +66,7 @@ namespace GRAL_2001
             Console.WriteLine("");
             Console.WriteLine("+------------------------------------------------------+");
             Console.WriteLine("|                                                      |");
-            string Info =     "+  > >         G R A L VERSION: 23.09            < <   +";
+            string Info =     "+  > >         G R A L VERSION: 23.11            < <   +";
             Console.WriteLine(Info);
             if (RunOnUnix)
             {
