520a734880f4ede47852d1d30334d0a4c7f550fd
Markus <markus.kuntner@tirol.gv.at>
2024-04-20 21:55:27 +0200

    AVX512 vector extension support

--- src/V-prognostic-microscale_1_Vec512.cs
+++ src/V-prognostic-microscale_1_Vec512.cs
@@ -1,7 +1,7 @@
 #region Copyright
 ///<remarks>
 /// <Graz Lagrangian Particle Dispersion Model>
-/// Copyright (C) [2019]  [Dietmar Oettl, Markus Kuntner]
+/// Copyright (C) [2024] [Markus Kuntner] Predecessor: [2019]  [Dietmar Oettl, Markus Kuntner]
 /// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
 /// the Free Software Foundation version 3 of the License
 /// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
