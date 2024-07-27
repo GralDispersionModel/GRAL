80adf991ab603cfbebac47c75ca1082f7f386841
Markus <markus.kuntner@tirol.gv.at>
2024-07-27 13:02:05 +0200

    Set version to RepoResBeta2

--- src/Program.cs
+++ src/Program.cs
@@ -66,7 +66,7 @@ namespace GRAL_2001
             Console.WriteLine("");
             Console.WriteLine("+------------------------------------------------------+");
             Console.WriteLine("|                                                      |");
-            string Info =     "+  > >         G R A L VERSION: 24.11Beta1       < <   +";
+            string Info =     "+  > >         G R A L VERSION: 24.11RepoResBeta2< <   +";
             Console.WriteLine(Info);
             if (RunOnUnix)
             {
